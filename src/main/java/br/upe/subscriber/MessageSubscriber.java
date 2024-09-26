59c0ae97ba55b6ca627209f63aaa2931f6cf1c28
ErickMartins153 <121458723+ErickMartins153@users.noreply.github.com>
2024-09-26 10:26:20 -0300

    feat: permitir que o usuário escolha o canal

--- src/main/java/br/upe/subscriber/MessageSubscriber.java
+++ src/main/java/br/upe/subscriber/MessageSubscriber.java
@@ -1,18 +1,13 @@
 package br.upe.subscriber;
 
-import jakarta.annotation.PostConstruct;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class MessageSubscriber {
 
-    @PostConstruct
-    public void init() {
-        System.out.println("Subscriber iniciado e aguardando mensagens...");
-    }
-
     public void handleMessage(String message) {
-
         System.out.println("Mensagem recebida: " + message);
+
     }
 }
--- src/main/java/br/upe/subscriber/RedisConfig.java
+++ src/main/java/br/upe/subscriber/RedisConfig.java
@@ -1,21 +1,31 @@
 package br.upe.subscriber;
 
+import io.lettuce.core.RedisConnectionException;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
+import org.springframework.util.ErrorHandler;
+
+import java.util.Scanner;
 
 @Configuration
 public class RedisConfig {
+    Scanner scanner = new Scanner(System.in);
+
 
     @Bean
     public RedisMessageListenerContainer redisContainer(RedisConnectionFactory connectionFactory,
                                                         MessageListenerAdapter listenerAdapter) {
+        System.out.println("Digite o canal que você deseja se inscrever: ");
+        String canal = scanner.nextLine();
+
         RedisMessageListenerContainer container = new RedisMessageListenerContainer();
         container.setConnectionFactory(connectionFactory);
-        container.addMessageListener(listenerAdapter, topic());
+        container.addMessageListener(listenerAdapter, topic(canal));
+
         return container;
     }
 
@@ -24,8 +34,7 @@ public class RedisConfig {
         return new MessageListenerAdapter(subscriber, "handleMessage");
     }
 
-    @Bean
-    public ChannelTopic topic() {
-        return new ChannelTopic("testa");
+    public ChannelTopic topic(String canal) {
+        return new ChannelTopic(canal);
     }
 }
